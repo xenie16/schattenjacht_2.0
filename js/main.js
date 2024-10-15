3bd2292933f2d1775ef5f0b7e13cd2da96a43b14
Haike <haike16@hotmail.com>
2024-10-16 00:10:29 +0200

    Moved the functions into the Wall class

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ let minDimension = Math.min(window.innerWidth, window.innerHeight);
 canvas.width = minDimension;
 canvas.height = minDimension;
 
+let cellWidth = canvas.width / 15;
+let cellHeight = canvas.height / 15;
+
+let map;
+const walls = [];
+
 class Wall {
    constructor({ position }) {
       this.position = position;
@@ -18,61 +24,49 @@ class Wall {
       ctx.fillStyle = "#B32D00";
       ctx.fillRect(this.position.x, this.position.y, this.cellWidth, this.cellHeight);
    }
-}
 
-let map;
+   static generateWalls(rows, columns, wallsNeeded) {
+      Wall.generate2DArray(rows, columns);
+      Wall.getRandomPosition(rows, columns, wallsNeeded);
 
-const walls = [];
+      map.forEach((row, rowIndex) => {
+         row.forEach((cell, columnIndex) => {
+            switch (cell) {
+               case '1':
+                  walls.push(new Wall({
+                     position: {
+                        x: cellWidth * columnIndex,
+                        y: cellHeight * rowIndex
+                     }
+                  }));
+                  break;
+            }
+         });
+      });
+   }
 
-let cellWidth = canvas.width / 15;
-let cellHeight = canvas.height / 15;
+   static generate2DArray(rows, columns) {
+      map = Array.from({ length: rows }, () =>
+         Array.from({ length: columns }, () => '0')
+      );
+   }
 
-generateWalls(15, 15, 10);
+   static getRandomPosition(rows, columns, wallsNeeded) {
+      for (let i = 0; i < wallsNeeded; i++) {
+         const x = Math.floor(Math.random() * columns);
+         const y = Math.floor(Math.random() * rows);
 
-map.forEach((row, rowIndex) => {
-   row.forEach((cell, columnIndex) => {
-      switch (cell) {
-         case '1':
-            walls.push(new Wall({
-               position: {
-                  x: cellWidth * columnIndex,
-                  y: cellHeight * rowIndex
-               }
-            }))
-            break;
-
-      }
-   })
-})
-
-walls.forEach((wall) => {
-   wall.draw();
-})
-
-
-
-function generateWalls(rows, columns, wallsNeeded) {
-   generate2DArray(rows, columns);
-   getRandomPosition(rows, columns, wallsNeeded);
-}
-
-function generate2DArray(rows, columns) {
-   map = Array.from({ length: rows }, () =>
-      Array.from({ length: columns }, () => '0')
-   );
-}
-
-function getRandomPosition(rows, columns, wallsNeeded) {
-   for (let i = 0; i < wallsNeeded; i++) {
-      const x = Math.floor(Math.random() * columns);
-      const y = Math.floor(Math.random() * rows);
-
-      if (map[y][x] === '0') {
-         map[y][x] = '1';
+         if (map[y][x] === '0') {
+            map[y][x] = '1';
+         }
       }
    }
 }
 
+Wall.generateWalls(15, 15, 10);
+
+walls.forEach((wall) => {
+   wall.draw();
+});
 
 console.log(map);
-
