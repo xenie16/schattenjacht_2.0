9ec8656ba2280b814340bea93c4b1ba8c78c1478
Haike <haike16@hotmail.com>
2024-10-17 20:21:55 +0200

    Main adapted to the new class structure

--- js/main.js
+++ js/main.js
@@ -1,70 +1,32 @@
 "use strict";
 
-class Wall {
-   constructor({ position }) {
-      this.canvas = document.getElementById("gameCanvas");
-      this.ctx = this.canvas.getContext("2d");
+import { CanvasManager } from "./CanvasManager.js";
+import { Wall } from "./Wall.js";
+// import { Player } from "./Player.js";
+// import { Enemy } from "./Enemy.js";
+// import { Treasure } from "./Treasure.js";
 
-      this.minDimension = Math.min(window.innerWidth, window.innerHeight);
-      this.canvas.width = this.minDimension;
-      this.canvas.height = this.minDimension;
+const canvas = document.getElementById("gameCanvas");
 
-      this.cellWidth = this.canvas.width / 15;
-      this.cellHeight = this.canvas.height / 15;
+const rows = 15;
+const cols = 15;
 
-      this.position = position;
-   }
+const numWalls = 10;
+const numPlayers = 1;
+const numEnemies = 5;
+const numTreasures = 5;
 
-   draw() {
-      this.ctx.fillStyle = "#B32D00";
-      this.ctx.fillRect(this.position.x, this.position.y, this.cellWidth, this.cellHeight);
-   }
+const wallColor = "#B32D00";
 
-   static generateWalls(rows, columns, wallsNeeded) {
-      const instance = new Wall({ position: { x: 0, y: 0 } });
-      let map = Wall.generate2DArray(rows, columns);
-      Wall.getRandomPosition(map, rows, columns, wallsNeeded);
+const canvasManager = new CanvasManager({ canvas, rows, cols });
+const ctx = canvasManager.getContext();
+const cellWidth = canvasManager.getCellWidth();
+const cellHeight = canvasManager.getCellHeight();
 
-      let walls = [];
 
-      map.forEach((row, rowIndex) => {
-         row.forEach((cell, columnIndex) => {
-            if (cell === '1') {
-               walls.push(new Wall({
-                  position: {
-                     x: instance.cellWidth * columnIndex,
-                     y: instance.cellHeight * rowIndex
-                  }
-               }));
-            }
-         });
-      });
+const wall = new Wall({ rows, cols, cellWidth, cellHeight, numWalls, wallColor, ctx });
+wall.generateEntities();
 
-      walls.forEach((wall) => {
-         wall.draw();
-      });
-
-      return walls;
-   }
-
-   static generate2DArray(rows, columns) {
-      return Array.from({ length: rows }, () =>
-         Array.from({ length: columns }, () => '0')
-      );
-   }
-
-   static getRandomPosition(map, rows, columns, wallsNeeded) {
-      for (let i = 0; i < wallsNeeded; i++) {
-         const x = Math.floor(Math.random() * columns);
-         const y = Math.floor(Math.random() * rows);
-
-         if (map[y][x] === '0') {
-            map[y][x] = '1';
-         }
-      }
-   }
-}
-
-const walls = Wall.generateWalls(15, 15, 10);
-
-console.log(walls);
+// const player = new Player({});
+// const enemy = new Enemy({});
+// const treasure = new Treasure({});
